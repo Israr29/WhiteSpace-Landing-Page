66db44fdce2737d4d6c64c4f9c2dccfd9cfde90e
Israr70 <im.israrahmed29@gmail.com>
2025-05-31 18:44:53 +0500

    just added some reposnsvieness

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
- const menuIcon = document.querySelector('#menu-icon');
- const navLinks = document.querySelector('.navLinks');
+let menulist = document.getElementById('menuList');
 
- menuIcon.onclick = () => {
-   menuIcon.classList.toggle('fa-xmark');
-   navLinks.classList.toggle('active');
- };
+menulist.style.maxHeight = '0px';
+
+function toggleMenu() {
+    if (menulist.style.maxHeight == '0px') {
+        menulist.style.maxHeight = '50vh'; // or any height that shows the menu
+    } else {
+        menulist.style.maxHeight = '0px';
+    }
+}
